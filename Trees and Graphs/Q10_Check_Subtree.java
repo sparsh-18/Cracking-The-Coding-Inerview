653be4af35dd723774466e95d93067f5c1c1b195
sparsh-18 <sparsh.red@gmail.com>
2021-06-20 23:34:07 +0530

    Q10 Check Subtree

--- Trees and Graphs/Q10_Check_Subtree.java
+++ Trees and Graphs/Q10_Check_Subtree.java
@@ -24,7 +24,27 @@ public class Q10_Check_Subtree {
         String t1Preorder = preorderWithNull(t1);
         String t2Preorder = preorderWithNull(t2);
 
-        return t1Preorder.indexOf(t2Preorder) != -1;
+        return t1Preorder.contains(t2Preorder);
+    }
+
+    //Another way where we find root of second tree then check rest of tree
+    public boolean isSubtree2(TreeNode t1, TreeNode t2) {
+        if (t2 == null) return true; // null tree is subtree
+
+        if (t1 == null) return false;
+
+        if (t1.value != t2.value) return isSubtree2(t1.left,t2) || isSubtree2(t1.right, t2);
+
+        return matchTree(t1,t2); // if head found
+    }
+
+    public boolean matchTree(TreeNode t1, TreeNode t2) {
+        if (t1==null && t2==null) return true; // both tree empty and checked
+        else if (t1==null || t2==null) return false; // any one of tree empty
+
+        if (t1.value != t2.value) return false;
+
+        return matchTree(t1.left,t2.left) && matchTree(t1.right,t2.right);
     }
 
     public static void main(String[] args) {
@@ -61,5 +81,6 @@ public class Q10_Check_Subtree {
         */
 
         System.out.println(ob.isSubtree(ob.t1,ob.t2));
+        System.out.println(ob.isSubtree2(ob.t1,ob.t2));
     }
 }
